59a253b2484faac3a0573d74566e0964592500c1
charu sharma <charu7636@gmail.com>
2025-09-11 15:21:50 +0530

    added

--- Wishlist/index.js
+++ Wishlist/index.js
@@ -1,17 +1,33 @@
 let todoInput= document.querySelector(".input");
 let addTodobutton= document.querySelector(".button");
+let showTodos=document.querySelector("todos-container");
 let toDo,
 todoList=[];
 
 
 //creating fuction to get unique id
 
+function uuid(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' .replace(/[xy]/g, function (param){
+        let number=Math.random()*16|0;
+        let randomNumber=param=='x'?number:(number& 0x3|0x8);
+        return randomNumber.toString(16);
+    });
+}
 
-addTodobutton=addEventListener("click",(e)=>{
+
+addTodobutton.addEventListener("click",(e)=>{
     e.preventDefault();
     toDo=todoInput.value;
     console.log(toDo);
     if (toDo.length>0){
-        todoList.push(id:)
+        todoList.push({id: uuid(), toDo: toDo, isCompleted : false});
     }
-})
+    renderTodoList(todoList);
+});
+
+function renderTodoList(todoList){
+    console.log(todoList);
+    showTodos.innerHTML=todoList.map(todo=>`<div><input type="checkbox"><label>${todo.todo}</label><button></button></div>`)
+}
+renderTodoList(todoList);
