b11e6fe14a8b60551593245d20d0274932a19c55
charu sharma <charu7636@gmail.com>
2025-09-11 12:58:53 +0530

    added

---
+++ Wishlist/index.js
@@ -0,0 +1,17 @@
+let todoInput= document.querySelector(".input");
+let addTodobutton= document.querySelector(".button");
+let toDo,
+todoList=[];
+
+
+//creating fuction to get unique id
+
+
+addTodobutton=addEventListener("click",(e)=>{
+    e.preventDefault();
+    toDo=todoInput.value;
+    console.log(toDo);
+    if (toDo.length>0){
+        todoList.push(id:)
+    }
+})
