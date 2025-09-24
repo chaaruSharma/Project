57bba7f837207f976072112ecb8b69a975db1c17
charu sharma <charu7636@gmail.com>
2025-09-24 23:20:17 +0530

    modified

--- prakash_sarkari/script.js
+++ prakash_sarkari/script.js
@@ -1,1 +1,33 @@
-let formData= document.getElementById("formData");
+let formData= document.querySelector(".form");
+let submitButton= document.querySelector(".button");
+let errormsg= document.querySelector(".error-message");
+let firstName, lastName, email, password,field;
+
+
+
+formData.addEventListener("keyup", (e)=>{
+    e.preventDefault();
+    field=e.target.dataset.key;
+    switch(field){
+        case "firstName":
+            firstName=e.target.value;
+            break;
+            case "lastName":
+            lastName=e.target.value;
+            break;
+            case "email":
+            email=e.target.value;
+            break;
+            case "password":
+            password=e.target.value;
+            break;
+            default:
+            lastName=firstName=email=password="";
+            break;
+
+    }
+});
+
+submitButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log(firstName, lastName, email, password);});
